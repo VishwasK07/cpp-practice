06ab6e37ff6a3d852b50824679352c551f010282
Vishwas K <kvishwas796@gmail.com>
2026-03-13 21:37:48 +0530

    Add files via upload

---
+++ switchcase.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"Enter two numbers: ";
+    cin>>n1>>n2;
+    cout<<"Enter an operator(+,-,*,%,/): ";
+    char op;
+    cin>>op;
+    switch(op){
+        case'+':
+            cout<<"Sum is: "<<n1+n2<<endl;
+            break;
+        case'-':
+            cout<<"Difference is: "<<n1-n2<<endl;
+            break;
+        case'*':
+            cout<<"Product is: "<<n1*n2<<endl;
+            break;
+        case'%':
+            cout<<"Remainder is: "<<n1%n2<<endl;
+            break;
+        case'/':
+            cout<<"Division is: "<<n1/n2<<endl;
+            break;
+        default:
+            cout<<"Enter a valid operator"<<endl;    
+    }
+}
