b560ffae939582b9e549979ad522885f9cbf2a17
Vishwas K <kvishwas796@gmail.com>
2026-09-03 12:19:09 +0530

    Add files via upload

---
+++ firstAndlast.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int firstPosition(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    int ans = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            ans = mid;
+            high = mid - 1;   // search left
+        }
+        else if (nums[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int lastPosition(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    int ans = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            ans = mid;
+            low = mid + 1;    // search right
+        }
+        else if (nums[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+
+    int first = firstPosition(nums, target);
+    int last = lastPosition(nums, target);
+
+    cout << "[" << first << ", " << last << "]";
+
+    return 0;
+}
